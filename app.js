d8207c3eac71ead739ec88bc4466215b2a465f12
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-09-29 19:49:48 +0530

    front added

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app=express();
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use(express.json());
-app.use(cors({origin:'http://localhost:3000',credentials:true}));
+app.use(cors({origin:'https://client-server-eta.vercel.app',credentials:true}));
+// app.use(cors({origin:'http://localhost:3000',credentials:true}));
 
 app.post('/login', async (req, res) => {
   console.log(req.body);
